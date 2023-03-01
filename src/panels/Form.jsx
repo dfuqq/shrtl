58bf18d26be20b1ca33929f946e56075a28ba915
dfuqq <dan38449@mail.ru>
2023-03-01 16:45:14 +0500

    Form Component UI

--- src/panels/Form.jsx
+++ src/panels/Form.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
-import { Panel } from '@vkontakte/vkui';
+import { Panel, Div, FormItem, Input, Button, Group } from '@vkontakte/vkui';
 
 const Form = () => {
-	return <Panel>Hello world</Panel>;
+	return (
+		<Panel>
+			<Div>
+				<FormItem top='Ссылка'>
+					<Group>
+						<Input
+							type='url'
+							placeholder='Введите ссылку'
+						/>
+						<Div
+							style={{
+								display: 'flex',
+								justifyContent: 'center',
+							}}>
+							<Button size='m'>Сократить</Button>
+						</Div>
+					</Group>
+				</FormItem>
+			</Div>
+		</Panel>
+	);
 };
 
 export default Form;
