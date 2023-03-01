1ba4fb27faf4e05956b3c9ada4c223225e9593e8
dfuqq <dan38449@mail.ru>
2023-03-01 20:05:20 +0500

    Skeleton for ShortLinks List

--- src/panels/ShortLinks.js
+++ src/panels/ShortLinks.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
-import { Panel, Div, Cell, Button } from '@vkontakte/vkui';
+import { Panel, Div, SimpleCell, Button } from '@vkontakte/vkui';
 
 const ShortLinks = () => {
+	const links = [];
+
+	if (!links?.length) return null;
+
 	return (
 		<Panel>
-			<Div></Div>
+			<Div>
+				{links.map((item) => (
+					<Div key={item.code}>
+						<SimpleCell>{item.original_link}</SimpleCell>
+						<SimpleCell>{item.full_short_link2}</SimpleCell>
+						<Button size='m'>Скопировать</Button>
+					</Div>
+				))}
+			</Div>
 		</Panel>
 	);
 };
