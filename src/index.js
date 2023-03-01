005f00d1388512e274a95a80529ac2f3f5edd5e3
dfuqq <dan38449@mail.ru>
2023-03-01 20:28:50 +0500

    Integrated Provider for permanent links save

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import bridge from '@vkontakte/vk-bridge';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
 // Init VK  Mini App
 bridge.send('VKWebAppInit');
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<PersistGate
+			persistor={persistor}
+			loading={null}>
+			<App />
+		</PersistGate>
 	</Provider>,
 	document.getElementById('root')
 );
