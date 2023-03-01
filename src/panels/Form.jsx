3aabdd46aa235c1b6b806facf05309c5dee01116
dfuqq <dan38449@mail.ru>
2023-03-01 16:51:00 +0500

    Input functionality and minor UI fixes

--- src/panels/Form.jsx
+++ src/panels/Form.jsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Panel, Div, FormItem, Input, Button, Group } from '@vkontakte/vkui';
 
 const Form = () => {
+	const [url, setUrl] = useState('');
+
+	const onChange = (e) => setUrl(e.currentTarget.value);
+
 	return (
 		<Panel>
 			<Div>
-				<FormItem top='Ссылка'>
-					<Group>
+				<Group>
+					<FormItem
+						top='Ссылка'
+						status={url ? 'valid' : 'error'}
+						bottom={
+							url
+								? 'Сейчас сократим...'
+								: 'Пожалуйста, введите ссылку'
+						}>
 						<Input
 							type='url'
 							placeholder='Введите ссылку'
+							name='url'
+							value={url}
+							onChange={onChange}
 						/>
-						<Div
-							style={{
-								display: 'flex',
-								justifyContent: 'center',
-							}}>
-							<Button size='m'>Сократить</Button>
-						</Div>
-					</Group>
-				</FormItem>
+					</FormItem>
+					<Div
+						style={{
+							display: 'flex',
+							justifyContent: 'center',
+						}}>
+						<Button size='m'>Сократить</Button>
+					</Div>
+				</Group>
 			</Div>
 		</Panel>
 	);
