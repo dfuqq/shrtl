ffe6b30883452e56d53bdbb29046519adc09d3a5
dfuqq <dan38449@mail.ru>
2023-03-01 20:25:56 +0500

    Created persister for using another provider

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,49 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
 
 import linkReducer from './slice/linkSlice';
+
+const persistConfig = {
+	key: 'root',
+	storage,
+};
+
+const rootReducer = combineReducers({ links: linkReducer });
+
+// Reducer для промежуточного сохранения store
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
 	reducer: {
 		links: linkReducer,
+		// Надстройка для избежания ошибок со стороны toolkit
+		middleware: (getDefaultMiddleware) =>
+			getDefaultMiddleware({
+				serializableCheck: {
+					ignoredActions: [
+						FLUSH,
+						REHYDRATE,
+						PAUSE,
+						PERSIST,
+						PURGE,
+						REGISTER,
+					],
+				},
+			}),
 	},
 });
+
+// persister — промежуточная оболочка для того, чтобы
+// использовать дополнительный провайдер
+export const persister = persistStore(store);
