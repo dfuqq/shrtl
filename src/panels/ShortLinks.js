42784378dd841f10dca2d870cf79d3f31ae20f5e
dfuqq <dan38449@mail.ru>
2023-03-01 20:07:20 +0500

    Added selector for received data

--- src/panels/ShortLinks.js
+++ src/panels/ShortLinks.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectLinks } from '../store/slice/linkSlice';
 
 import { Panel, Div, SimpleCell, Button } from '@vkontakte/vkui';
 
 const ShortLinks = () => {
-	const links = [];
+	const links = useSelector(selectLinks);
 
 	if (!links?.length) return null;
 
