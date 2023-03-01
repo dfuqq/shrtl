6258c08e9e1e22eb1833041dde1e665b3aa7bdb0
dfuqq <dan38449@mail.ru>
2023-03-02 00:11:47 +0500

    Copy function and some UI Changes

--- src/panels/ShortLinks/ShortLinks.js
+++ src/panels/ShortLinks/ShortLinks.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectLinks } from '../../store/slice/linkSlice';
 
@@ -6,7 +6,14 @@ import { Panel, Div, Cell, Button, Link, Text } from '@vkontakte/vkui';
 import './ShortLinks.css';
 
 const ShortLinks = () => {
+	const [copiedUrl, setCopiedUrl] = useState(null);
 	const links = useSelector(selectLinks);
+
+	const copyToClipboard = (url) => {
+		navigator.clipboard.writeText(url).then(() => {
+			setCopiedUrl(url);
+		});
+	};
 
 	if (!links?.length) return null;
 
@@ -28,8 +35,25 @@ const ShortLinks = () => {
 							className='Cell'
 							after={
 								<Div>
-									<Button mode='overlay_outline'>
-										Скопировать
+									<Button
+										mode={
+											copiedUrl === item.full_short_link2
+												? 'overlay_primary'
+												: 'overlay_outline'
+										}
+										disabled={
+											copiedUrl === item.full_short_link2
+												? true
+												: false
+										}
+										onClick={() =>
+											copyToClipboard(
+												item.full_short_link2
+											)
+										}>
+										{copiedUrl === item.full_short_link2
+											? 'Скопировано'
+											: 'Скопировать'}
 									</Button>
 								</Div>
 							}>
