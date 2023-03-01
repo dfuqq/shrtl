0a8a64dfd600b970f76d7d768c405a00608874e2
dfuqq <dan38449@mail.ru>
2023-03-01 20:00:13 +0500

    Created Component for rendering short links

---
+++ src/panels/ShortLinks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Panel, Div, Cell, Button } from '@vkontakte/vkui';
+
+const ShortLinks = () => {
+	return (
+		<Panel>
+			<Div></Div>
+		</Panel>
+	);
+};
+
+export default ShortLinks;
